61daca5797b0f30a0c8818c555c01b544840d888
pridato <davidarroyo25052004@gmail.com>
2024-07-08 16:13:45 +0200

    creado login sin funcionar

--- TapSecureApp/screens/LoginScreen.tsx
+++ TapSecureApp/screens/LoginScreen.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { IconFaceId } from '@tabler/icons-react-native';
 
-const LoginScreen = ({navigation}:any) => {
+const LoginScreen = ({ navigation }: any) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -15,7 +16,7 @@ const LoginScreen = ({navigation}:any) => {
       alert('Credenciales inválidas');
     }
   };
-  
+
   return (
     <View style={styles.container}>
       {/* texto olvidar contraseña */}
@@ -28,9 +29,9 @@ const LoginScreen = ({navigation}:any) => {
       <Image source={require('../assets/contrasena.gif')} style={styles.icon} />
 
       {/* inputs de correo y contraseña */}
-      <TextInput style={styles.input}  placeholder="correo@mail.com" placeholderTextColor="#c4c4c4" value={email} onChangeText={setEmail} keyboardType="email-address" autoCapitalize="none" />
-      <TextInput style={styles.input}  placeholder="Contraseña" placeholderTextColor="#c4c4c4" value={password} onChangeText={setPassword} secureTextEntry />
-      
+      <TextInput style={styles.input} placeholder="correo@mail.com" placeholderTextColor="#c4c4c4" value={email} onChangeText={setEmail} keyboardType="email-address" autoCapitalize="none" />
+      <TextInput style={styles.input} placeholder="Contraseña" placeholderTextColor="#c4c4c4" value={password} onChangeText={setPassword} secureTextEntry />
+
       {/* boton de login */}
       <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
         <Text style={styles.loginButtonText}>Log in</Text>
@@ -43,28 +44,40 @@ const LoginScreen = ({navigation}:any) => {
           <Text style={styles.createAccountLink}> Crear cuenta</Text>
         </TouchableOpacity>
       </View>
+
+      {/* Boton de faceId */}
+      <View style={styles.faceContainer}>
+        <TouchableOpacity style={{alignContent: 'center', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}} onPress={() => alert('FaceId')}>
+          <IconFaceId size={40} stroke="#c4c4c4" />
+          <Text style={styles.useFaceId}>Accede mostrando tu cara</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  // contenedor principal de la vista
   container: {
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 30,
     backgroundColor: '#fff',
   },
+  // icono gif del candado
   icon: {
-    width: 350,
-    height: 350,
+    width: 300,
+    height: 300,
     alignSelf: 'center',
     marginBottom: 20,
   },
+  // contenedor principal forgotpassword
   forgotPasswordContainer: {
     position: 'absolute',
     top: 80,
     right: 30,
   },
+  // texto de olvidar contraseña
   forgotPassword: {
     alignSelf: 'flex-end',
     marginBottom: 20,
@@ -73,6 +86,7 @@ const styles = StyleSheet.create({
     color: '#c4c4c4',
     fontSize: 14,
   },
+  // inputs email y contraseña
   input: {
     height: 40,
     borderColor: '#c4c4c4',
@@ -81,6 +95,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     marginBottom: 25,
   },
+  // boton de login
   loginButton: {
     backgroundColor: '#000',
     height: 60,
@@ -90,6 +105,7 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     marginTop: 15,
   },
+  // texto del boton de login
   loginButtonText: {
     color: '#fff',
     fontSize: 20
@@ -106,6 +122,18 @@ const styles = StyleSheet.create({
     color: '#33CCCC',
     fontSize: 14,
   },
+  faceContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 40,
+  },
+  useFaceId: {
+    color: '#c4c4c4',
+    fontSize: 14,
+    marginLeft: 10,
+    textDecorationLine: 'underline',
+  }
+
 });
 
 export default LoginScreen;
